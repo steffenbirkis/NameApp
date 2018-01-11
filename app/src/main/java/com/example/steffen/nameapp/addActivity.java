89bc1b4fcc3dc8bea78327719c3a5e60adfe2d3b
Schnappi <sondrelfc@hotmail.com>
2018-01-11 14:09:29 +0100

    yo

--- app/src/main/java/com/example/steffen/nameapp/addActivity.java
+++ app/src/main/java/com/example/steffen/nameapp/addActivity.java
@@ -1,8 +1,23 @@
 package com.example.steffen.nameapp;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+
 /**
  * Created by Steffen on 11.01.2018.
  */
 
 public class addActivity {
-}
+
+    /** Sjekke om det er kamera på enheten */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // Enheten har kamera
+            return true;
+        } else {
+            // Enheten har ikke kamera
+            return false;
+        }
+    }
+
+} //class
