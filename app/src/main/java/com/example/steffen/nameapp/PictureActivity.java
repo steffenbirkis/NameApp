cd43332bfcff13bc2791b0819b30a823d0da8826
steffenbirkis <birkisproductions@gmail.com>
2018-01-11 13:01:55 +0100

    first

--- app/src/main/java/com/example/steffen/nameapp/PictureActivity.java
+++ app/src/main/java/com/example/steffen/nameapp/PictureActivity.java
@@ -5,13 +5,27 @@ package com.example.steffen.nameapp;
  */
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class PictureActivity extends AppCompatActivity  {
 @Override
 protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_picture);
-    
+
+    GridView gridview = (GridView) findViewById(R.id.gridview);
+    gridview.setAdapter(new ImageAdapter(this));
+
+    gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> parent, View v,
+                                int position, long id) {
+            Toast.makeText(PictureActivity.this, "" + position,
+                    Toast.LENGTH_SHORT).show();
+        }
+    });
 }
 
 
