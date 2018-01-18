97a5089e539bfd36731dbff9f035aaa81958b77e
steffenbirkis <birkisproductions@gmail.com>
2018-01-18 13:13:03 +0100

    Funket ikke

--- app/src/main/java/com/example/steffen/nameapp/PictureActivity.java
+++ app/src/main/java/com/example/steffen/nameapp/PictureActivity.java
@@ -4,7 +4,6 @@ package com.example.steffen.nameapp;
  * Created by Steffen on 11.01.2018.
  */
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,8 +11,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
-
-import static com.example.steffen.nameapp.addActivity.REQUEST_IMAGE_CAPTURE;
 
 public class PictureActivity extends AppCompatActivity  {
 @Override
@@ -32,32 +29,21 @@ protected void onCreate(Bundle savedInstanceState) {
         }
     });
 }
-
-public void onPickImage(View view) {
-
-
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
-        }
-    }
-
-
-
-
-
-
-
+public void addNew(View view){
+    Intent myIntent = new Intent(view.getContext(), addActivity.class);
+    startActivityForResult(myIntent, 0);
 }
+
+
+    }
+
+
+
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/steffen/nameapp/addActivity.java
+++ app/src/main/java/com/example/steffen/nameapp/addActivity.java
@@ -1,5 +1,6 @@
 package com.example.steffen.nameapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 /**
@@ -30,9 +32,39 @@ public class addActivity extends AppCompatActivity {
         }
     }*/
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap mImageBitmap;
+    private String mCurrentPhotoPath;
+    private ImageView mImageView;
 
 
 
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_add);
+            this.mImageView = (ImageView)this.findViewById(R.id.imageView1);
+            Button photoButton = (Button) this.findViewById(R.id.button1);
+            photoButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            });
+        }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            mImageView.setImageBitmap(photo);
+        }
+    }
 }
+
+
+
+
 
 
