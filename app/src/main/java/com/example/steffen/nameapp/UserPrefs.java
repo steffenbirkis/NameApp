12c1ad3cbf866a15b354651099bd3fdb75bdc419
Kevin294 <kevin.arnesen@hotmail.com>
2018-01-29 14:10:29 +0100

    asd

--- app/src/main/java/com/example/steffen/nameapp/UserPrefs.java
+++ app/src/main/java/com/example/steffen/nameapp/UserPrefs.java
@@ -1,5 +1,7 @@
 package com.example.steffen.nameapp;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -30,6 +32,14 @@ public class UserPrefs extends PreferenceActivity {
             PreferenceManager manager = getPreferenceManager();
             manager.setSharedPreferencesName("user_prefs");
             addPreferencesFromResource(R.xml.userprefs);
+            Preference myPref = (Preference) findPreference("myKey");
+            myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent myIntent = new Intent(preference.getContext(), PrefPic.class);
+                    startActivityForResult(myIntent, 0);
+                    return true;
+                }
+            });
         }
     }
 }
