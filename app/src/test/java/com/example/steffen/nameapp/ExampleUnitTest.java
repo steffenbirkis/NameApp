ad3c60cbf5d075841b97faca6503df3c14eafb3a
Schnappi <sondrelfc@hotmail.com>
2018-01-29 14:29:38 +0100

    yo

--- app/src/test/java/com/example/steffen/nameapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/steffen/nameapp/ExampleUnitTest.java
@@ -9,6 +9,19 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+/**
+ * TODO:
+ * Teste at score blir oppdatert riktig
+ * Teste utvidelse av bildelisten når et bilde blir lagt til
+ * Teste at en person blir opprettet riktig
+ * Teste om settings åpner seg automatisk når appen åpnes for første gang
+ * Teste spillet
+ * Teste alle knappene
+ * Teste at alle aktivitetene starter riktig
+ * Teste toLowerCase
+ * 
+ */
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
