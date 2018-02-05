d5ed3bd5027b9f582f9aee4a8743eb7e88e48bbb
Kevin294 <kevin.arnesen@hotmail.com>
2018-02-05 12:49:44 +0100

    internaltest

--- app/src/main/java/com/example/steffen/nameapp/PrefPic.java
+++ app/src/main/java/com/example/steffen/nameapp/PrefPic.java
@@ -1,11 +1,7 @@
 package com.example.steffen.nameapp;
 
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -14,12 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 /**
  * Created by kevin on 29-Jan-18.
  */
@@ -27,9 +17,7 @@ import java.io.IOException;
 public class PrefPic extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int REQUEST_TAKE_PHOTO = 1;
     private ImageView mImageView;
-    private String mCurrentPhotoPath;
     private Bitmap imageBitmap;
 
 
@@ -37,9 +25,7 @@ public class PrefPic extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addtopref);
         mImageView = findViewById(R.id.imgview);
-        //directory();
-        //viewImg("imgdir");
-        viewImg2();
+        viewImg();
         Button butt = findViewById(R.id.button2);
         butt.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -71,69 +57,18 @@ public class PrefPic extends AppCompatActivity {
         BitmapDrawable bd = new BitmapDrawable(getResources(), imageBitmap);
         Bitmap bp = bd.getBitmap();
         mImageView.setImageBitmap(bp);
-        //saveToInternalStorage(bp);
-        save2(bp);
+        saveimg(bp);
         finish();
-
     }
 
-    private String saveToInternalStorage(Bitmap bitmapImage) {
-        ContextWrapper cw = new ContextWrapper(getApplicationContext());
-        // path to /data/data/yourapp/app_data/imageDir
-        File directory = cw.getDir("imgdir", Context.MODE_PRIVATE);
-        // Create imageDir
-        File mypath = new File(directory, "profile.jpg");
-
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(mypath);
-            // Use the compress method on the BitMap object to write image to the OutputStream
-            bitmapImage.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                fos.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-        return directory.getAbsolutePath();
-    }
-
-    private void viewImg(String path) {
-        Bitmap b = null;
-        try {
-            File f = new File(path, "profile.jpg");
-            b = BitmapFactory.decodeStream(new FileInputStream(f));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        if (b != null) {
-            mImageView.setImageBitmap(b);
-        }
-    }
-
-    private void directory() {
-        String fname = "imageDir";
-        File folder = getFilesDir();
-        File f = new File(folder, fname);
-        if (!f.exists()) {
-            f.mkdir();
-        }
-    }
-
-    public void save2(Bitmap bitmap){
+    public void saveimg(Bitmap bitmap) {
         new ImageSaver(this).
                 setFileName("myImage.png").
                 setDirectoryName("images").
                 save(bitmap);
     }
 
-    public void viewImg2(){
+    public void viewImg() {
         Bitmap bitmap = new ImageSaver(this).
                 setFileName("myImage.png").
                 setDirectoryName("images").
